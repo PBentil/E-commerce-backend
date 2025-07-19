b71a4cf78f4fcfb261285005bc2a04ada7ec96fd
PBentil <paapabentil122@gmail.com>
2025-07-19 18:50:18 +0000

    bugfixes

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ import authRoutes from "./routes/auth.routes.js";
 import testRoute from "./routes/test.route.js";
 import profileRoutes from "./routes/profile.routes.js"
 import adminRoutes from "./routes/admin.routes.js"
+import cors from "cors";
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.get("/", (req, res) => {
     res.send("api is working!");
